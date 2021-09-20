888f3973c17333120f08dc5d36bdee82f13c1270
shahd1995913 <shahdalkhatib95@gmail.com>
2021-09-20 10:55:33 +0300

    update

--- modules/aboutus/Aboutuspage.js
+++ modules/aboutus/Aboutuspage.js
@@ -2,66 +2,62 @@
 
 const mongoose = require("mongoose");
 
-
-
-
-
-let modelAboutUs ;
+let modelAboutUs;
 const AboutSchema = new mongoose.Schema({
     // name: String
     // the basic structure for object
     PersonName: String,
     image: String,
     Description: String,
-    email:String
+    email: String
 
 
-}); 
+});
 
-modelAboutUs = mongoose.model("AboutSchema",AboutSchema);
+modelAboutUs = mongoose.model("AboutSchema", AboutSchema);
 
 
 console.log(modelAboutUs)
 
 async function getaboutUsInfo() {
 
-  
+
 
     const person1 = new modelAboutUs({
         PersonName: 'Shahd',
         image: '',
         Description: 'Book about the art in The past',
-        email:'shhad@gmail.com'
+        email: 'shhad@gmail.com'
     });
     const person2 = new modelAboutUs({
         PersonName: 'Shahd',
         image: '',
         Description: 'Book about the art in The past',
-        email:'shhad@gmail.com'
+        email: 'shhad@gmail.com'
     });
     const person3 = new modelAboutUs({
         PersonName: 'Shahd',
         image: '',
-        Description: 'Book about the art in The past' ,
-        email:'shhad@gmail.com'
+        Description: 'Book about the art in The past',
+        email: 'shhad@gmail.com'
     });
     const person4 = new modelAboutUs({
         PersonName: 'Shahd',
         image: '',
         Description: 'Book about the art in The past',
-        email:'shhad@gmail.com'
+        email: 'shhad@gmail.com'
     });
     const person5 = new modelAboutUs({
         PersonName: 'Shahd',
-        image: `https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80` , 
+        image: `https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80`,
         Description: 'Book about the art in The past',
-        email:'shhad@gmail.com'
+        email: 'shhad@gmail.com'
     });
     const person6 = new modelAboutUs({
         PersonName: 'Shahd',
         image: '',
         Description: 'Book about the art in The past',
-        email:'shhad@gmail.com'
+        email: 'shhad@gmail.com'
     });
     // we use save method to save data 
 
@@ -74,31 +70,47 @@ async function getaboutUsInfo() {
 }
 
 
-function updateAbotUsHandler(req, res) {
+// function updateAbotUsHandler(req, res) {
+//     const id = req.params.id;
+//     const { image, Description, PersonName } = req.body;
+
+//     modelAboutUs.findByIdAndUpdate(id, { image, PersonName, Description }, (err, result) => {
+//         modelAboutUs.find({ ownerEmail: email }, (err, result) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 res.send(result);
+//             }
+//         })
+//     })
+// }
+
+async function updateAbotUsHandler(req, res) {
     const id = req.params.id;
-    const { image, Description, PersonName } = req.body;
+    const { image, Description, PersonName, email } = req.body;
+   
 
-    modelAboutUs.findByIdAndUpdate(id, { image, PersonName, Description }, (err, result) => {
-        modelAboutUs.find({ ownerEmail: email }, (err, result) => {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                res.send(result);
-            }
-        })
+    await modelAboutUs.findByIdAndUpdate(id, { image, PersonName, Description })
+
+    modelAboutUs.find({ }, (err, result) => {
+        if (err) {
+            console.log('Error !');
+        }
+        else {
+            console.log(result);
+            res.send(result);
+        }
     })
+
 }
-
-
-
 
 
 function getAboutHandler(r, resp) {
     // const x=r.query.email ;
     // const email= r.query.email;
-//    console.log(x)
-  modelAboutUs.find({  }, (err, result) => {
+    //    console.log(x)
+    modelAboutUs.find({}, (err, result) => {
         if (err) {
 
             console.log(err);
@@ -116,4 +128,4 @@ function getAboutHandler(r, resp) {
 
 }
 
-module.exports = {getAboutHandler ,updateAbotUsHandler }
+module.exports = { getAboutHandler, updateAbotUsHandler }
